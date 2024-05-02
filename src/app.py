ab1d0af6d43d1ed59561214a5f83422e02746742
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-02 09:14:07 +0800

    add working email sender

--- src/app.py
+++ src/app.py
@@ -1,14 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
 
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    email = os.environ.get('EMAIL')
+    password = os.environ.get('PASSWORD')
 
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = email
+    app.config['MAIL_PASSWORD'] = password
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+
+    mail.init_app(app)
     db.init_app(app)
 
     with app.app_context():
--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template, redirect, url_for, request
-from .app import db
+from .app import db, mail
 from .models import Subscribe
 from flask import send_from_directory
+from flask_mail import Message
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
+
+email_sender = os.getenv('EMAIL')
 
 def init_app(app):
 
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.route('/subscribe_newsletter', methods=['POST'])
+    def send_email():
+        recipient = request.form.get('email')
+
+        msg = Message("Hello", sender=email_sender, recipients=[recipient])
+        msg.body = "This is a message from flask"
+        mail.send(msg)
+        print(recipient)
+        print("Email sent")
+
+        return redirect(url_for('index'))
     
